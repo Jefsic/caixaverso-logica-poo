dbe3b6945d82d9d65212b573c29a2d9802b64a1b
Jefferson <jefsic@gmail.com>
2025-09-25 21:30:17 -0300

    Add files via upload

---
+++ Exercicio2/ContaCorrente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class ContaCorrente : Conta
+    {
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente.");
+                return false;
+            }
+        }
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c = new ContaCorrente();
+
+            Console.WriteLine("Digite o valor a depositar:");
+            c.Depositar(double.Parse(Console.ReadLine()));
+          
+            Console.WriteLine("Digite o valor a sacar:");
+            c.Sacar(double.Parse(Console.ReadLine()));
+              
+            Console.WriteLine($"Saldo atual: {c.ConsultarSaldo()}");
+            Console.WriteLine("Tecle qualquer tecla para encerrar...");
+            Console.ReadKey();
+        }
+    }
+}
