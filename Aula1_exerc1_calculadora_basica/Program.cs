5ce12dd549819d47c268830fd0a8a66856eebd70
Jefferson <jefsic@gmail.com>
2025-09-25 22:34:52 -0300

    Add files via upload

---
+++ Aula1_exerc1_calculadora_basica/Program.cs
@@ -0,0 +1,32 @@
+﻿// Exercício 1: Calculadora básica
+// Autor: Jefferson Siqueira Costa
+// Data: 11/09/2025
+
+using System;
+using System.Numerics;
+
+namespace Aula1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 8;
+        int b = 2;
+
+        Console.Clear();
+        Console.WriteLine($"O resultado da soma é: {(a + b).ToString("F2")}");
+        Console.WriteLine($"O resultado da subtração é: {(a - b).ToString("F2")}");
+        Console.WriteLine($"O resultado da multiplicação é: {(a * b).ToString("F2")}");
+        Console.WriteLine($"O resultado da divisão é: {(a / b).ToString("F2")}");
+
+        Console.WriteLine("\nDigite qualquer tecla para continuar...");
+        Console.ReadKey();
+
+    }
+
+}
+
+
+
+
---
+++ Aula1_exerc2_conversor_temperatura/Program.cs
@@ -0,0 +1,42 @@
+﻿// Exercício 2: Conversor de temperatura
+// Autor: Jefferson Siqueira Costa
+// Data: 11/09/2025
+
+using System;
+using System.Numerics;
+
+namespace Aula1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double temperaturaInicial;
+        double temperaturaF;
+        double temperaturaK;
+
+        //fórmula matemática para conversão: Fahrenheit = (°C × 9 / 5) +32
+        //fórmula matemática para a conversão Kelvin = °C + 273.15
+
+        Console.Clear();
+        Console.WriteLine("Digite a temperatura em graus Celsius: ");
+        temperaturaInicial = double.Parse(Console.ReadLine());
+        temperaturaF = (temperaturaInicial * (9 / 5)) +32;
+        temperaturaK = (temperaturaInicial + 273.15);
+
+        Console.Clear();
+        Console.WriteLine($"Conversão Fahrenheit: {temperaturaInicial} graus Celsius equivale a {temperaturaF} graus Fahrenheit!");
+        Console.WriteLine();
+        Console.WriteLine($"Conversão Kelvin: {temperaturaInicial} graus Celsius equivale a {temperaturaK} graus Kelvin!");
+
+        Console.WriteLine("\nDigite qualquer tecla para encerrar...");
+        Console.ReadKey();
+
+    }
+
+}
+
+
+
+
+
